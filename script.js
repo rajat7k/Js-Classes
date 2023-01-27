9b792d2a922b0a5e9ec4a983fd706a8c9c46f516
Rajat <kaushikrajat777.com>
2023-01-27 17:52:27 +0530

    ..

--- script.js
+++ script.js
@@ -2,43 +2,133 @@
 
 class Set{
 
-
+    constructor(setObject){
+        this.set_id=setObject.set_id;
+        this.set_name=setObject.set_name;
+        this.set_type=setObject.set_type;
+    }
 
 }
+
+
+
+class Assignment_set extends Set{
+    constructor(setObject){
+        super(setObject);
+    }
+    show_questions_list(){
+        console.log(`Question list  of ${setObject.set_name}`)
+    }
+    show_question_detail(){
+        console.log(`Question detail  of ${setObject.set_name}`)
+    }
+    submit_code(){
+        console.log(`Submit Code of ${setObject.set_name}`)
+    }
+    run_the_code(){
+        console.log(`Run Code of ${setObject.set_name}`)
+    }
+    show_submission_list(){
+        console.log(`Show submission List of ${setObject.set_name}`)
+    }
+    show_the_hint_of_question(){
+        console.log(`Show Hint of ${setObject.set_name} Question`)
+    }
+    show_discussion_list(){
+        console.log(`Show Discussion List  of ${setObject.set_name}`)
+    }
+}
+
+class Common_methods_of_Coding_Practise extends Assignment_set{
+    constructor(setObject){
+        super(setObject);
+    }
+    show_solutoin(){
+        console.log(`Show solution  of ${setObject.set_name}`)
+    }
+    reset_code(){
+        console.log(`Reset Code of ${setObject.set_name}`)
+    }
+    save_code(){
+        console.log(`Save Code  of ${setObject.set_name}`)
+    }
+}
+
 
 class MCQ_set extends Set{
 
+    constructor(setObject){
+        super(setObject);
+        this.instruction_url=setObject.instruction_url;
+        this.questions_url=setObject.questions_url;
+
+    }
+    fetch_mcq_instructions(){
+
+    }
+    fetch_question(){
+
+    }
+    submit_answer(){
+
+    }
+    skip_question(){
+
+    }
+    see_answer(){
+
+    }
+
 }
 
-class Coding_set extends Set{
-
+class Coding_set extends Common_methods_of_Coding_Practise {
+    constructor(setObject){
+        super(setObject);
+    }
+   
 }
 
-class Practise_set extends Set{
+class Practise_set extends Common_methods_of_Coding_Practise {
+    constructor(setObject){
+        super(setObject);
+    }
 
 }
 
 class Learning_set extends Set{
 
+    constructor(setObject){
+        super(setObject);
+    }
+
+    vedio_details(){
+
+    }
+    discusion_list(){
+
+    }
+    notes_list(){
+
+    }
+    mark_as_completed(){
+
+    }
+    submit_feedback(){
+
+    }
+    ask_the_doubt(){
+
+    }
 
 }
 
-class Assignment_set extends Set{
-
-}
 
 
 
 
 class Topics{
 
-    sets_type={
-        mcq_set:[],
-        coding_set:[],
-        practise_set:[],
-        learning_set:[],
-        assignments_set:[],
-    };
+    sets_list=[];
     constructor(topicObject){
         this.topic_id=topicObject.topic_id;
         this.topic_name=topicObject.topic_name;
@@ -49,24 +139,23 @@ class Topics{
             let setObject=topicObject.sets_list[setId];
             if(new_set_type==="mcq_set"){
                 new_set=new MCQ_set(setObject);
-                this.sets_type.mcq_set.push(new_set);
             }
             else if(new_set_type==="coding_set"){
                 new_set=new Coding_set(setObject);
-                this.sets_type.coding_set.push(new_set);
             }
             else if(new_set_type==="practise_set"){
                 new_set=new Practise_set(setObject);
-                this.sets_type.practise_set.push(new_set);
+           
             }
             else if(new_set_type==="learning_set"){
                 new_set=new Learning_set(setObject);
-                this.sets_type.learning_set.push(new_set);
+               
             }
             else{
                 new_set=new Assignment_set(setObject);
-                this.sets_type.assignments_set.push(new_set);
+              
             }
+            this.sets_list.push(new_set);
         }
     }
 
@@ -116,6 +205,9 @@ let trackObject={
                             set_id:1,
                             set_type:"mcq_set",
                             set_name:"classes_MCQ",
+                            instruction_url:"xyz",
+                            questions_url:"xyz",
+
                         },
                         {
                             set_id:2,
@@ -134,6 +226,16 @@ let trackObject={
                         },
                         {
                             set_id:5,
+                            set_type:"assignments_set",
+                            set_name:"classes_assignments",
+                        },
+                        {
+                            set_id:6,
+                            set_type:"learning_set",
+                            set_name:"classes_Learning",
+                        },
+                        {
+                            set_id:7,
                             set_type:"assignments_set",
                             set_name:"classes_assignments",
                         },
